8b541f3ebf0951dc95449e17cf097a7f8eeecfe0
thisaramax20 <thisarathejana20@gmail.com>
2025-03-26 10:18:47 +0530

    fix .env file

--- interview_prep/firebase/client.ts
+++ interview_prep/firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_CLIENT_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
   authDomain: "interview-prep-next.firebaseapp.com",
   projectId: "interview-prep-next",
   storageBucket: "interview-prep-next.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_CLIENT_SENDER_ID,
-  appId: process.env.FIREBASE_CLIENT_APP_ID,
-  measurementId: process.env.FIREBASE_CLIENT_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
